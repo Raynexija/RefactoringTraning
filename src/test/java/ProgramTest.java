4e3e434eadef055c2f052409e1ffebaa77528069
TheRaynex <rafael.luedtke@web.de>
2022-05-10 09:44:13 +0200

    Added Unit Test

---
+++ src/test/java/ProgramTest.java
@@ -0,0 +1,25 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+
+
+public class ProgramTest {
+
+    @Test
+    public void test(){
+        Movie m1 = new Movie("movie1", 1);
+        Movie m2 = new Movie("movie2", 2);
+        Rental r1 = new Rental(m1, 10);
+        Rental r2 = new Rental(m2, 5);
+        Customer c1 = new Customer("joe");
+        c1.addRental(r1);
+        c1.addRental(r2);
+        String result = "Rental Record for joe\n" +
+                "\tTitle\t\tDays\tAmount\n" +
+                "\tmovie1\t\t10\t30.0\n" +
+                "\tmovie2\t\t5\t4.5\n" +
+                "Amount owed is 34.5\n" +
+                "You earned 3 frequent renter points";
+        System.out.println(c1.statement());
+        assertEquals(result, c1.statement());
+    }
+}
