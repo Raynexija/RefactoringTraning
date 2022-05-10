e84485bf6fcd3f77f3f393e6f5b7819fbf236b0a
TheRaynex <rafael.luedtke@web.de>
2022-05-10 10:22:46 +0200

    Extract Method

--- src/main/java/Rental.java
+++ src/main/java/Rental.java
@@ -1,13 +1,16 @@
 class Rental {
     private Movie movie;
     private int daysRented;
+
     public Rental(Movie newmovie, int newdaysRented) {
         movie = newmovie;
         daysRented = newdaysRented;
     }
+
     public int getDaysRented() {
         return daysRented;
     }
+
     public Movie getMovie() {
         return movie;
     }
@@ -30,6 +33,14 @@ class Rental {
                 break;
         }
         return thisAmount;
+    }
+
+    public int getFrequentRenterPoints() {
+        // add bonus for a two day new release rental
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1) {
+            return 2;
+        }
+        return 1;
     }
 
 }
