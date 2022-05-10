573f7d822e7f912942ee265d3ca0748f6e6534e9
TheRaynex <rafael.luedtke@web.de>
2022-05-10 11:02:00 +0200

    Move Method

--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -1,18 +1,23 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
-
 public class ProgramTest {
+    private Customer c1 = new Customer("joe");
 
-    @Test
-    public void test(){
+    @Before
+    public void init() {
         Movie m1 = new Movie("movie1", 1);
         Movie m2 = new Movie("movie2", 2);
         Rental r1 = new Rental(m1, 10);
         Rental r2 = new Rental(m2, 5);
-        Customer c1 = new Customer("joe");
         c1.addRental(r1);
         c1.addRental(r2);
+    }
+
+    @Test
+    public void testASCII() {
         String result = "Rental Record for joe\n" +
                 "\tTitle\t\tDays\tAmount\n" +
                 "\tmovie1\t\t10\t30.0\n" +
@@ -21,12 +26,16 @@ public class ProgramTest {
                 "You earned 3 frequent renter points";
         System.out.println(c1.statement() + "\n\n");
         assertEquals(result, c1.statement());
-        String httpResult ="<H1>Rentals for <EM>joe</EM></ H1><P>\n" +
+    }
+
+    @Test
+    public void testHTML() {
+        String htmlResult = "<H1>Rentals for <EM>joe</EM></ H1><P>\n" +
                 "movie1: 30.0<BR>\n" +
                 "movie2: 4.5<BR>\n" +
                 "<P>You owe <EM>34.5</EM><P>\n" +
                 "On this rental you earned <EM>3</EM> frequent renter points<P>";
         System.out.println(c1.htmlStatement());
-        assertEquals(httpResult, c1.htmlStatement());
+        assertEquals(htmlResult, c1.htmlStatement());
     }
 }
