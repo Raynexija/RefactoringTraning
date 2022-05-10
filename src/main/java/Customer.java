954b23bb3448106ac584879b2cb0851621e485d6
TheRaynex <rafael.luedtke@web.de>
2022-05-10 10:46:52 +0200

    Added htmlStatement() Method + Test for it

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -50,7 +50,7 @@ class Customer {
         return result;
     }
 
-    private int getTotalFrequentRenterPoints(){
+    private int getTotalFrequentRenterPoints() {
         int result = 0;
         Enumeration eRentals = rentals.elements();
         while (eRentals.hasMoreElements()) {
@@ -60,5 +60,23 @@ class Customer {
         return result;
     }
 
+    public String htmlStatement() {
+        Enumeration eRentals = rentals.elements();
+        String result = "<H1>Rentals for <EM>" + getName() + "</EM></ H1><P>\n";
+        while (eRentals.hasMoreElements()) {
+            Rental each = (Rental) eRentals.nextElement();
+            //show figures for each rental
+            result += each.getMovie().getTitle() + ": " +
+                    String.valueOf(each.getCharge()) + "<BR>\n";
+        }
+        //add footer lines
+        result += "<P>You owe <EM>" + String.valueOf(getTotalCharge()) +
+                "</EM><P>\n";
+        result += "On this rental you earned <EM>" +
+                String.valueOf(getTotalFrequentRenterPoints()) +
+                "</EM> frequent renter points<P>";
+        return result;
+    }
 }
+
     
--- src/test/java/ProgramTest.java
+++ src/test/java/ProgramTest.java
@@ -19,7 +19,14 @@ public class ProgramTest {
                 "\tmovie2\t\t5\t4.5\n" +
                 "Amount owed is 34.5\n" +
                 "You earned 3 frequent renter points";
-        System.out.println(c1.statement());
+        System.out.println(c1.statement() + "\n\n");
         assertEquals(result, c1.statement());
+        String httpResult ="<H1>Rentals for <EM>joe</EM></ H1><P>\n" +
+                "movie1: 30.0<BR>\n" +
+                "movie2: 4.5<BR>\n" +
+                "<P>You owe <EM>34.5</EM><P>\n" +
+                "On this rental you earned <EM>3</EM> frequent renter points<P>";
+        System.out.println(c1.htmlStatement());
+        assertEquals(httpResult, c1.htmlStatement());
     }
 }
